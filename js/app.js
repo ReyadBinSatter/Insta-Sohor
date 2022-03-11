e38228071ac172d6778b571724ac4b6a55043bcc
ReyadBinSatter <rbsreyad@gmail.com>
2022-03-12 05:02:49 +0600

    final code

--- js/app.js
+++ js/app.js
@@ -145,6 +145,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   document.getElementById( "liked" ).textContent = "";
+  document.getElementById( "qna" ).textContent = "";
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -154,6 +155,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   document.getElementById( "reported" ).textContent = "";
+  document.getElementById( "qna" ).textContent = "";
     const reportedPosts = getReportedPosts();
  
     
